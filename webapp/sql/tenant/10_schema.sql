c69215e159fffce3b4933e425d66c85e85170186
masami <msihima0304@gmail.com>
2023-11-23 14:58:07 +0900

    インデックス追加

--- webapp/sql/tenant/10_schema.sql
+++ webapp/sql/tenant/10_schema.sql
@@ -30,3 +30,5 @@ CREATE TABLE player_score (
   created_at BIGINT NOT NULL,
   updated_at BIGINT NOT NULL
 );
+CREATE INDEX idx_tenant_competition ON player_score(tenant_id, competition_id);
+CREATE INDEX idx_tenant_player_competition ON player_score(tenant_id, competition_id, player_id);
